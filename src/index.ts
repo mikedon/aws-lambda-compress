a60536a4e124d5df61fb75bc6a0a8583aa8c79a7
mdonovan <michael.donovan@danteinc.com>
2015-11-01 15:59:12 -0500

    refactor logic into classes

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,20 @@ class Dependency {
 	constructor(name: string){
 		this.name = name;		
 	}
+	
+	buildLocation(){
+		let dependencyLocationTokens = require.resolve(this.name).split(path.sep);									
+		let doneBuildingPath = false;
+		let i = 0;
+		while(!doneBuildingPath){
+			this.location += `${dependencyLocationTokens[i]}${path.sep}`;
+			if(dependencyLocationTokens[i - 1] === "node_modules"){
+				doneBuildingPath = true;
+			}else{
+				i++;	
+			}								
+		}
+	}
 }
 
 class LambdaFunction {
@@ -25,62 +39,60 @@ class LambdaFunction {
 	directory: string;
 	name: string;	
 	
-	constructor(name: string){
+	constructor(name: string, srcDir: string){
 		this.name = name;
+		this.directory = path.join(process.cwd(), srcDir, this.name);
+	}
+	
+	async analyzeDependencies(excludes: string[]){
+		if(!excludes){
+			excludes = [];
+		}
+		let dependencies = await analyze({ // await
+			target: this.directory
+		});
+								
+		for(let dependency in dependencies){			
+			if(excludes.indexOf(dependency) < 0){					
+				let lfDependency = new Dependency(dependency);
+				this.dependencies.push(lfDependency);
+				lfDependency.buildLocation();	
+			}
+		}
+	}
+	
+	createArchive(outputDirectory: string){
+		let stream = fs.createWriteStream(`${outputDirectory}/${this.name}.zip`);
+		stream.on("error", err => {
+			console.log(err);				
+			process.exit(-1);
+		});
+		let archive = archiver.create('zip', {});
+		archive.pipe(stream);		
+		archive.directory(path.relative("", this.directory));
+		this.dependencies.forEach(dependency => {						
+			archive.directory(path.relative("", dependency.location));
+		});		
+		archive.finalize();
 	}
 }
 
-// this can't be let because of cli.ts?
 export async function compress(srcDir: string, excludes : string[], outputDir: string){	
 	try{	
 		let lambdaFunctions: LambdaFunction[] = [];
 		let dependencies: {[key: string]: string[]} = {};
-		let files = await readDir(srcDir); //yield
+		let files = await readDir(srcDir); //await
 		let directories: string[] = files.filter((file)=>{
 			return !fs.statSync(path.join(srcDir, file)).isFile();
 		});
 		for(let directory of directories){		
-			let lambdaFunction = new LambdaFunction(directory);
-			lambdaFunction.directory = path.join(process.cwd(), srcDir, directory);					
-			let dependencies = await analyze({ // await
-				target: lambdaFunction.directory
-			});						
-			for(let dependency in dependencies){
-				if(excludes.indexOf(dependency) < 0){					
-					let lfDependency = new Dependency(dependency);
-					lambdaFunction.dependencies.push(lfDependency);
-					let dependencyLocationTokens = require.resolve(dependency).split(path.sep);									
-					let doneBuildingPath = false;
-					let i = 0;
-					while(!doneBuildingPath){
-						lfDependency.location += `${dependencyLocationTokens[i]}${path.sep}`;
-						if(dependencyLocationTokens[i - 1] === "node_modules"){
-							doneBuildingPath = true;
-						}else{
-							i++;	
-						}								
-					}							
-				}
-			}		
-			//TODO move to LambdaFunction class
+			let lambdaFunction = new LambdaFunction(directory, srcDir);					
+			await lambdaFunction.analyzeDependencies(excludes);								
 			let outputDirectory = `${process.cwd()}/${outputDir}`;
 			await makeDirectory(outputDirectory); //await
-			let output = fs.createWriteStream(`${outputDirectory}/${lambdaFunction.name}.zip`);
-			output.on("error", err => {
-				console.log(err);				
-				process.exit(-1);
-			});						
-			let archive = archiver.create('zip', {});
-			archive.pipe(output);		
-			archive.directory(path.relative("", lambdaFunction.directory));
-			lambdaFunction.dependencies.forEach(dependency => {	
-				console.log(dependency);		
-				archive.directory(path.relative("", dependency.location));
-			});		
-			archive.finalize();		
+			lambdaFunction.createArchive(outputDir);											
 		}	
 	}catch(err){
-		console.log("hit the catch");
 		console.error(err);
 	}		
 };
